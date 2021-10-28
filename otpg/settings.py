4d98f79ab59de6b02f3db2735ad23da360f4fce8
nikiforniket <nikeet@exiverprojects.com>
2021-10-28 12:03:41 +0530

    Added new settings

--- otpg/settings.py
+++ otpg/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$6w=zo4!91p39k!%qyl71g!s0=ie3a6=e)2s@1_n^2fy&&98p#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
